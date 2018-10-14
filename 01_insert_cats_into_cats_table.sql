e366ca0ea15348c66acad1e447bf5d9268572d44
Raymond Willey <raymond_willey@me.com>
2018-10-14 13:58:40 -0400

    Codealong Complete

--- 01_insert_cats_into_cats_table.sql
+++ 01_insert_cats_into_cats_table.sql
@@ -1,2 +1,2 @@
-'''INSERT INTO cats (name, age, breed) VALUES ('Lil\' Bub', 5, 'American Shorthair');'''
-'''INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');'''
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 5, "American Shorthair");
+INSERT INTO cats (name, age, breed) VALUES ("Hannah", 1, "Tabby");
